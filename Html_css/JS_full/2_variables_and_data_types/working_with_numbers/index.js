c38b13d02399f85419df1a9f102b177c10042652
Aleksandr <stepanetsaleksandr@gmail.com>
2021-10-06 22:57:56 +0300

    12

--- Html_css/JS_full/2_variables_and_data_types/working_with_numbers/index.js
+++ Html_css/JS_full/2_variables_and_data_types/working_with_numbers/index.js
@@ -1,1 +1,8 @@
-
+let a = 4,
+  b = 2;
+console.log("a:", a);
+console.log("b:", b);
+console.log("a - b =", a - b);
+console.log("a + b =", a + b);
+console.log("a / b =", a / b);
+console.log("a * b =", a * b);
