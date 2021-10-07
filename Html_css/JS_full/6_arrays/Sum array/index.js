b01977810b575ce6150b4852fbf8c1f44222662b
Aleksandr <stepanetsaleksandr@gmail.com>
2021-10-07 13:51:54 +0300

    213

--- Html_css/JS_full/6_arrays/Sum array/index.js
+++ Html_css/JS_full/6_arrays/Sum array/index.js
@@ -1,17 +1,19 @@
-"use strict";
-
 /**
  * @param {number[]} arr
- * @return {number[]}
+ * @return {number}
  */
-function getArrayBounds(arr) {
+function getSum(arr) {
   if (!Array.isArray(arr)) {
     return null;
   }
-
-  return [arr.length, arr[0], arr.length - 1];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+  }
+  return sum;
 }
 // examples
-console.log(getArrayBounds([1, 10, 9, 11])); // ==> [4, 1, 11]
-console.log(getArrayBounds(10, 12, 14)); // ==> null
-console.log(getArrayBounds([1])); // ==> [1, 1, 1]
+getSum([1, 10, -10, 4]); // ==> 5
+getSum([1]); // ==> 1
+getSum([-8, 8]); // ==> 0
+getSum(10, 12, 14); // ==> null
