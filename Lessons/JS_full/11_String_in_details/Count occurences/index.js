01bf22a0026b52b66d28185f43cbfa423af6ac91
Aleksandr <stepanetsaleksandr@gmail.com>
2021-11-19 23:28:31 +0200

    1231

---
+++ Lessons/JS_full/11_String_in_details/Count occurences/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-param-reassign */
+// 1 через search ищем совпадение
+// 2 каждое совпадение плюсуем в счетчик
+// 3 если строка не передана заменяем первый аргумент на ''
+// 4 если второй аргумент пустой - ретурним null
+// 5 не работает
+
+// 1 через split разделяем строку по маске на елементы массива
+// 2 количество елементов масива -1 и есть количество вхождений
+// 3 если строка не передана заменяем первый аргумент на ''
+// 4 если второй аргумент пустой - ретурним null
+
+const countOccurrences = (text, str) => {
+  if (str === undefined || str === '') return null;
+  if (text === undefined) text = '';
+
+  const newtxt = text.split(str);
+  const res = newtxt.length - 1;
+
+  return res;
+};
+
+console.log(countOccurrences('', ''));
