961f45e245124a4b38057739f047a1269cd12611
Aleksandr <stepanetsaleksandr@gmail.com>
2021-11-22 13:05:39 +0200

    asdasd

--- Lessons/JS_full/12_Practice_with_string/Очистка данных о транзакциях/index.js
+++ Lessons/JS_full/12_Practice_with_string/Очистка данных о транзакциях/index.js
@@ -1,11 +1,32 @@
-const cleanTransactionsList = str => {
-  if (typeof str === 'string') {
-    const strArr = str.split('');
-    strArr.reverse();
-    const result = strArr.join('');
-    return result;
+// 1. Проходим по елементам массива
+// 2. Каждый проверяем на тип, если строка модифицируем ее под условия
+// 3. Если в троке есть буква,  пушим null
+// 4. Если число просто пушим в новый масив
+// 5. При каждо пуше добавляем $ и два знака после запятой
+// 6. Фильтруем итог удаляя null
+
+const modString = str => {
+  if (str.match(/[a-z]/)) {
+    return null;
   }
-  return null;
+  return '$' + (+str.trim()).toFixed(2);
 };
 
-console.log(cleanTransactionsList(324));
+const cleanTransactionsList = arr => {
+  let resArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof arr[index] === 'string') {
+      resArr.push(modString(arr[index]));
+    } else resArr.push('$' + arr[index].toFixed(2));
+  }
+  resArr = resArr.filter(function (el) {
+    return el != null;
+  });
+  return resArr;
+};
+
+// console.log(cleanTransactionsList(arr));
+
+const arr = ['  1.9    ', '16.4', 17, ' 1 dollar '];
+
+console.log(cleanTransactionsList(arr));
