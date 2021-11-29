c1231ba29d15f5abcdfdc7492a50826e8a0057cf
Aleksandr <stepanetsaleksandr@gmail.com>
2021-11-29 14:34:47 +0200

    123

---
+++ Lessons/JS_full/21_DOM/Поиск элемента на странице/index.js
@@ -0,0 +1,10 @@
+export function getTitleElement() {
+  const titleElement = document.querySelector(`.title`);
+  console.dir(titleElement);
+  return titleElement;
+}
+export function getInputElement() {
+  const inputElement = document.querySelector(`input[type = 'text']`);
+  console.dir(inputElement);
+  return inputElement;
+}
